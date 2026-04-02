27445660afcb243510ca59abb61530c7e22b6f0c
Your Name <you@example.com>
2026-04-02 20:58:29 +0200

    Add Cargo-style freeform experiment

--- script/main.js
+++ script/main.js
@@ -1,6 +1,7 @@
 import { setupHomePage } from "./home.js";
 import { setupHomeEditor } from "./home-editor.js";
 import { setupAlbumEditor } from "./album/editor.js";
+import { setupFreeformExperimentPage } from "./freeform-experiment.js";
 import { setupLightbox } from "./lightbox.js";
 
 const bootstrap = async () => {
@@ -10,12 +11,16 @@ const bootstrap = async () => {
     await setupHomeEditor();
     setupHomePage();
     await setupAlbumEditor();
+    await setupFreeformExperimentPage();
     setupLightbox();
   } finally {
     if (body?.classList.contains("home-page")) {
       body.classList.add("is-ready");
     }
     if (body?.classList.contains("album-page")) {
+      body.classList.add("is-ready");
+    }
+    if (body?.classList.contains("freeform-experiment-page")) {
       body.classList.add("is-ready");
     }
   }
